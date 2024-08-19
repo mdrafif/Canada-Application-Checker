91251f1e45d0105bdb3719f2ba9617f9a4c07781
mdrafif <mdrafi69656@gmail.com>
2024-08-19 12:39:41 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+        
+        function redirect(){
+            var test =  document.querySelector('#passport');
+            if(test.value == "123456"){
+                    window.location.href="blank.html";
+            }else if(test.value == "passport number"){
+               window.location.href="login location"
+            }                                      
+            else{
+                 alert("invalid Passport")
+            }
+   
+              }
+           
+               
+             
+           
+         
+          
