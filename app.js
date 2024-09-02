bcca34ab4ec35919029649dbbc67bed8c145c3ed
mdrafif <mdrafi69656@gmail.com>
2024-09-02 10:37:55 +0600

    Update app.js
    done

--- app.js
+++ app.js
@@ -3,8 +3,8 @@
             var test =  document.querySelector('#passport');
             if(test.value == "123456"){
                     window.location.href="blank.html";
-            }else if(test.value == "passport number"){
-               window.location.href="login location"
+            }else if(test.value == "EM0465896"){
+               window.location.href="rostom.html"
             }                                      
             else{
                  alert("invalid Passport")
